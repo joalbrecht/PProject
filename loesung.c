1172a5e8d4ab94f2bbbc163decd0942467bba325
joalbrecht <jonas1albrecht@gmail.com>
2021-08-25 20:47:37 +0200

    cut out not used shit

--- loesung.c
+++ loesung.c
@@ -11,20 +11,11 @@
 
 int BUFFER_SIZE = 64;
 static int nodeCounter = 0;
-uint8_t **adjacencyMatrix = NULL;
 uint64_t string_buffer_size = START_BUFFER;
 //uint64_t *marks = NULL;
 struct Node *nodeList = NULL;
 int DEBUG = 0;
 
-void printMatrix(uint8_t **memory, int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            printf("%d ", memory[i][j]);
-        }
-        printf("\n");
-    }
-}
 
 struct Node {
     char* name;
@@ -33,7 +24,7 @@ struct Node {
     uint32_t mark;
 
 };
-//sorts a given List, used for sorting neighbournodes list
+//sorts a given List, used for sorting neighbournodes list //Static weil schneller
 static void sortList(char** List, int n) {
     char *tmp;
     for (int i = 0; i < n - 1; i++) {
@@ -46,14 +37,7 @@ static void sortList(char** List, int n) {
         }
     }
 }
-void addNeighbour(struct Node targetNode, char* newNode){
-    
-    targetNode.neighbourList = realloc(targetNode.neighbourList, (targetNode.neighbour_count+1) * sizeof(char*));
-    targetNode.neighbourList[targetNode.neighbour_count] = newNode;
-    sortList(targetNode.neighbourList,targetNode.neighbour_count);
-    targetNode.neighbour_count++;
-    //free(tmp);
-}
+
 
 void addNode(char* node){ 
     struct Node tmp ;
@@ -64,31 +48,6 @@ void addNode(char* node){
     nodeList[nodeCounter].mark = 0;
     nodeCounter++;
     
-}
-void reallocMatrix(int size) {
-    if (DEBUG) printf("reallocMatrix() called. size: %d\n", size);
-    int previousSize = size-5;
-    adjacencyMatrix = (uint8_t**) realloc(adjacencyMatrix, size * sizeof(uint8_t*));
-    if (DEBUG) printf("matrix** reallocated. Adress: %p\n", (void*) adjacencyMatrix);
-    // TODO error catch allocation error
-    for (int i = 0; i < previousSize; i++) {
-        adjacencyMatrix[i] = realloc(adjacencyMatrix[i], size * sizeof(uint8_t));
-        if (DEBUG) printf("[%d] matrix* reallocated. Adress: %p\n", i, (void*) adjacencyMatrix[i]);
-    }
-    for (int k = previousSize; k < size; k++) {
-        adjacencyMatrix[k] = calloc(size, sizeof(uint8_t));
-    }
-    if (DEBUG) printf("reallocating matrix done.\n");
-    
-    for(int i = 0; i < size; i++) { 
-        for (int j = 0; j < size; j++) {
-            if((i < previousSize) && (j < previousSize)){
-                continue;
-            }
-            adjacencyMatrix[i][j] = 0;
-        }
-        
-    }
 }
 
 
@@ -145,12 +104,9 @@ int goStep(int currentNode){
 
     int idNextNode;
     int neighbourStep;
-    //if(DEBUG)printf("Buffer Size bei nachbarlist: %d\n", BUFFER_SIZE);
-    
-    //printf("Neighbors von %s: %d\n",nodeList[currentNode].name, nodeList[currentNode].neighbour_count);
     
     neighbourStep = nodeList[currentNode].mark % nodeList[currentNode].neighbour_count;
-    //printf("neighbourstep: %d\n",neighbourStep);
+
     idNextNode = isDuplicate(nodeList[currentNode].neighbourList[neighbourStep]);
     /*
     for(int i = 0; i < neighbour_count;i++) {
@@ -158,7 +114,6 @@ int goStep(int currentNode){
     } */
     //idNextNode = 0 ;
     nodeList[currentNode].mark++;
-    //free(neighbourList);
     return idNextNode;
 }
 
@@ -224,16 +179,11 @@ void getNode(char *input){
                 if(nodeCounter + 3 > BUFFER_SIZE){
                     if(DEBUG)printf("muss reallocated werden: weil: NodeCounter: %d, BUFFERSIZE: %d\n",nodeCounter,BUFFER_SIZE);
                     BUFFER_SIZE = BUFFER_SIZE +5;
-                    reallocMatrix(BUFFER_SIZE);
                     
-                    if(DEBUG)printf("Matrix reallocated\n");
                     if(DEBUG) printf("nodeList Ptr: %p \n", (void*) nodeList);
                     nodeList = (struct Node*) realloc(nodeList, BUFFER_SIZE * sizeof(struct Node));
                     if(DEBUG)printf("nodeList reallocated\n");
-                    /*if (DEBUG) printf("marks Ptr: %p \n", (void*) marks);
-                    marks = (uint64_t*) realloc(marks, BUFFER_SIZE * sizeof(uint64_t));
-                    if(DEBUG)printf("marks reallocated\n");*/
-                    //printf("here2\n");
+                   
                 }
                 
                 addNode(node);
@@ -260,9 +210,7 @@ void getNode(char *input){
                     
                 }
             }
-            /*if(input[i] == '\n'){
-                marks[idFirstNode] = 0;
-            }*/
+           
             nodeSize = 0;
             if (input[i] == '-' ) {
                 markerMode = 1;
@@ -288,35 +236,18 @@ void getNode(char *input){
 
 int main (void) {
      
-    //INIT MATRIX 
-    adjacencyMatrix = calloc(BUFFER_SIZE, sizeof(uint8_t*));
-    for (int i = 0; i < BUFFER_SIZE; i++) {
-        adjacencyMatrix[i] = calloc(BUFFER_SIZE, sizeof(uint8_t));
-    }
-    // INIT MATRIX END
-    
+  
+   
+
     char *input_ptr = NULL; //malloc(START_BUFFER * sizeof(char*));
     nodeList = malloc(BUFFER_SIZE * sizeof(struct Node));
-    //marks = calloc(BUFFER_SIZE, sizeof(unsigned int));
+   
     
     size_t len = 0;
-    //printf("Here\n");
-    //input_ptr[0] = 'A';
+
     while (getline(&input_ptr, &len, stdin) != -1) {
         if(DEBUG)printf("input main: %s",input_ptr);
-        //memset(input_ptr, 0, strlen(input_ptr) + 1); // check whether +1 is enough to reset whole string
-        //getline(&input_ptr, &len, stdin);
-        /*while (!(input_ptr[strlen(input_ptr) - 1] == '\n')){
-            
-            string_buffer_size = string_buffer_size * 2; // double buffer size
-            if(DEBUG)printf("Reallocate: new string buffer: %d\n", (int)string_buffer_size);
-            char *tmp = malloc(string_buffer_size * sizeof(char));
-            getline(&tmp, &len, stdin);
-            input_ptr = realloc(input_ptr, string_buffer_size * sizeof(char) + string_buffer_size / 2);
-            input_ptr = strcat(input_ptr, tmp);
-            free(tmp);
-        }
-        */
+       
         if (input_ptr[0] == 'A') { //exits the reading loop to get into the StartCondition mode
             break;
         }
