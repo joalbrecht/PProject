fbf629f481946c0fdb6075c8a63d767d67db7ce7
joalbrecht <jonas1albrecht@gmail.com>
2021-08-25 20:36:15 +0200

    renamed

--- loesung.c
+++ loesung.c
@@ -7,12 +7,14 @@
 
 #define START_BUFFER 256
 
-int BUFFER_SIZE = 1028;
+
+
+int BUFFER_SIZE = 64;
 static int nodeCounter = 0;
 uint8_t **adjacencyMatrix = NULL;
 uint64_t string_buffer_size = START_BUFFER;
-uint64_t *marks = NULL;
-char **nodeList = NULL;
+//uint64_t *marks = NULL;
+struct Node *nodeList = NULL;
 int DEBUG = 0;
 
 void printMatrix(uint8_t **memory, int size) {
@@ -24,7 +26,45 @@ void printMatrix(uint8_t **memory, int size) {
     }
 }
 
+struct Node {
+    char* name;
+    char** neighbourList;
+    int neighbour_count;
+    uint32_t mark;
 
+};
+//sorts a given List, used for sorting neighbournodes list
+static void sortList(char** List, int n) {
+    char *tmp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (strcmp(List[i], List[j]) > 0) {
+                tmp = List[i];
+                List[i] = List[j];
+                List[j] = tmp;
+            }
+        }
+    }
+}
+void addNeighbour(struct Node targetNode, char* newNode){
+    
+    targetNode.neighbourList = realloc(targetNode.neighbourList, (targetNode.neighbour_count+1) * sizeof(char*));
+    targetNode.neighbourList[targetNode.neighbour_count] = newNode;
+    sortList(targetNode.neighbourList,targetNode.neighbour_count);
+    targetNode.neighbour_count++;
+    //free(tmp);
+}
+
+void addNode(char* node){ 
+    struct Node tmp ;
+    tmp.name = malloc((strlen(node) + 1) * sizeof(char));
+    strcpy(tmp.name, node);
+    tmp.neighbourList = malloc(1*sizeof(char*));
+    nodeList[nodeCounter] = tmp;
+    nodeList[nodeCounter].mark = 0;
+    nodeCounter++;
+    
+}
 void reallocMatrix(int size) {
     if (DEBUG) printf("reallocMatrix() called. size: %d\n", size);
     int previousSize = size-5;
@@ -56,78 +96,69 @@ void reallocMatrix(int size) {
 int isDuplicate(char *node) { 
     int i;
     for (i = 0; i < nodeCounter; i++) {
-        if (strcmp(nodeList[i], node) == 0) {
+        if (strcmp(nodeList[i].name, node) == 0) {
             return i;
         }
     }
     return -1;
 }
 
-//adds a Node to the global NodeList
-void addNode(char *node) {
-    char *tmp = malloc((strlen(node) + 1) * sizeof(char));
-    strcpy(tmp, node);
-    nodeList[nodeCounter] = tmp;
-    nodeCounter++;
-}
+
 
 //adds an entry of 1 into the column and row in the global adjacency Matrix
 void addEdge(int IDbase, int IDadd) {
     if (DEBUG) {
-        printf("Adding Edge between %d|%s and %d|%s\n", IDbase, nodeList[IDbase], IDadd, nodeList[IDadd]);
+        
+    
+    printf("Adding Edge between %d|%s and %d|%s\n", IDbase, nodeList[IDbase].name, IDadd, nodeList[IDadd].name);
+    printf("Neighbourcount von base %s: %d\n",nodeList[IDbase].name, (int)nodeList[IDbase].neighbour_count);
+    
+    for(int i = 0; i < (int)nodeList[IDbase].neighbour_count; i++){
+        printf("%s, ", nodeList[IDbase].neighbourList[i]);
     }
-    adjacencyMatrix[IDbase][IDadd] = 1;
-    adjacencyMatrix[IDadd][IDbase] = 1;
+    }
+    //Add Edge on Base
+    //nodeList[IDbase].neighbourList = realloc(nodeList[IDbase].neighbourList, (nodeList[IDbase].neighbour_count+1) * sizeof(char*));
+    nodeList[IDbase].neighbourList[nodeList[IDbase].neighbour_count] = nodeList[IDadd].name;
+    sortList(nodeList[IDbase].neighbourList,nodeList[IDbase].neighbour_count);
+    nodeList[IDbase].neighbour_count++;
+    if(DEBUG){
+        printf("Neighbourcount von base %s: %d\n",nodeList[IDbase].name, (int)nodeList[IDbase].neighbour_count);
+    for(int i = 0; i < (int)nodeList[IDbase].neighbour_count; i++){
+        printf("%s, ", nodeList[IDbase].neighbourList[i]);
+    }
+    printf("Neighbourcount von add %s: %d\n",nodeList[IDadd].name, (int)nodeList[IDadd].neighbour_count);
+    }
+    //Add Edge on Add
+    //nodeList[IDadd].neighbourList = realloc(nodeList[IDadd].neighbourList, (nodeList[IDadd].neighbour_count+1) * sizeof(char*));
+    nodeList[IDadd].neighbourList[nodeList[IDadd].neighbour_count] = nodeList[IDbase].name;
+    sortList(nodeList[IDadd].neighbourList,nodeList[IDadd].neighbour_count);
+    nodeList[IDadd].neighbour_count++;
+    
 }
 
-//sorts a given List, used for sorting neighbournodes list
-static void sortList(char **List, int n) {
-    char *tmp;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (strcmp(List[i], List[j]) > 0) {
-                tmp = List[i];
-                List[i] = List[j];
-                List[j] = tmp;
-            }
-        }
-    }
-}
 
-//gets the number of neighbour nodes, and also modifies the given list of neighbour nodes in the function
-int getNeighbourNodes(int nodeId, char** neighbourList) {
-    int neighbour_count = 0;
-    for (int i = 0; i < nodeCounter; i++)
-    {
-        if (adjacencyMatrix[nodeId][i] == 1)
-        {
-            neighbourList[neighbour_count] = nodeList[i];
-            neighbour_count++;
-        }
-    }
-    sortList(neighbourList, neighbour_count);
-    return neighbour_count;
-}
+
 
 // simulates a step of the ant. calculates where to go by getting the amount of neighbours and the current mark on the Node
 int goStep(int currentNode){
 
     int idNextNode;
     int neighbourStep;
-    if(DEBUG)printf("Buffer Size bei nachbarlist: %d\n", BUFFER_SIZE);
-    char** neighbourList = malloc(BUFFER_SIZE * sizeof(char*));
-    int neighbour_count = getNeighbourNodes(currentNode, neighbourList);
-    if (DEBUG)printf("Neighbors von %s: %d\n",nodeList[currentNode], neighbour_count);
+    //if(DEBUG)printf("Buffer Size bei nachbarlist: %d\n", BUFFER_SIZE);
     
-    neighbourStep = marks[currentNode] % neighbour_count;
-   
-    idNextNode = isDuplicate(neighbourList[neighbourStep]);
+    //printf("Neighbors von %s: %d\n",nodeList[currentNode].name, nodeList[currentNode].neighbour_count);
+    
+    neighbourStep = nodeList[currentNode].mark % nodeList[currentNode].neighbour_count;
+    //printf("neighbourstep: %d\n",neighbourStep);
+    idNextNode = isDuplicate(nodeList[currentNode].neighbourList[neighbourStep]);
     /*
     for(int i = 0; i < neighbour_count;i++) {
         printf("%s, ", neighbourList[i]);
     } */
-    marks[currentNode]++;
-    free(neighbourList);
+    //idNextNode = 0 ;
+    nodeList[currentNode].mark++;
+    //free(neighbourList);
     return idNextNode;
 }
 
@@ -179,7 +210,8 @@ void getNode(char *input){
             }
             node[currentNodeIndex] = input[i];
             if (input[i] == '\n') {
-                marks[idFirstNode] = atoi(node);
+                //marks[idFirstNode] = atoi(node);
+                nodeList[idFirstNode].mark = atoi(node);
                 break;
             }
         }
@@ -193,28 +225,28 @@ void getNode(char *input){
                     if(DEBUG)printf("muss reallocated werden: weil: NodeCounter: %d, BUFFERSIZE: %d\n",nodeCounter,BUFFER_SIZE);
                     BUFFER_SIZE = BUFFER_SIZE +5;
                     reallocMatrix(BUFFER_SIZE);
-                    if(DEBUG){
-                        printf("Matrix nach reallocaten: \n");
-                        printMatrix(adjacencyMatrix,BUFFER_SIZE);
-                    }
+                    
                     if(DEBUG)printf("Matrix reallocated\n");
                     if(DEBUG) printf("nodeList Ptr: %p \n", (void*) nodeList);
-                    nodeList = (char**) realloc(nodeList, BUFFER_SIZE * sizeof(char*));
+                    nodeList = (struct Node*) realloc(nodeList, BUFFER_SIZE * sizeof(struct Node));
                     if(DEBUG)printf("nodeList reallocated\n");
-                    if (DEBUG) printf("marks Ptr: %p \n", (void*) marks);
+                    /*if (DEBUG) printf("marks Ptr: %p \n", (void*) marks);
                     marks = (uint64_t*) realloc(marks, BUFFER_SIZE * sizeof(uint64_t));
-                    if(DEBUG)printf("marks reallocated\n");
+                    if(DEBUG)printf("marks reallocated\n");*/
                     //printf("here2\n");
                 }
                 
                 addNode(node);
-                if(DEBUG)printf("node Added Successfully\n");
+                if(DEBUG)printf("node: %s Added Successfully\n", node);
             }
             if (isFirstNode == 0) {
                 idCurrentNode = isDuplicate(node);
 
                 if(idFirstNode!=idCurrentNode){
+                    if(DEBUG)printf("Neighbors von %s vorher: %d\n",nodeList[idFirstNode].name,nodeList[idFirstNode].neighbour_count);
                     addEdge(idFirstNode, idCurrentNode);
+                    if(DEBUG)printf("Neighbors von %s nachher: %d\n",nodeList[idFirstNode].name,nodeList[idFirstNode].neighbour_count);
+
                 }
                 
             }
@@ -228,9 +260,9 @@ void getNode(char *input){
                     
                 }
             }
-            if(input[i] == '\n'){
+            /*if(input[i] == '\n'){
                 marks[idFirstNode] = 0;
-            }
+            }*/
             nodeSize = 0;
             if (input[i] == '-' ) {
                 markerMode = 1;
@@ -262,19 +294,14 @@ int main (void) {
         adjacencyMatrix[i] = calloc(BUFFER_SIZE, sizeof(uint8_t));
     }
     // INIT MATRIX END
-    if(DEBUG){
-        printf("Matrix: \n");
-        printMatrix(adjacencyMatrix,BUFFER_SIZE);
-    }
-     
     
-    char *input_ptr = malloc(START_BUFFER * sizeof(char));
-    nodeList = malloc(BUFFER_SIZE * sizeof(char*));
-    marks = calloc(BUFFER_SIZE, sizeof(unsigned int));
+    char *input_ptr = malloc(START_BUFFER * sizeof(char*));
+    nodeList = malloc(BUFFER_SIZE * sizeof(struct Node));
+    //marks = calloc(BUFFER_SIZE, sizeof(unsigned int));
     
     size_t len = 0;
     //printf("Here\n");
-    input_ptr[0] = 0;
+    input_ptr[0] = 'A';
     while (strcmp(input_ptr, "")) {
         if(DEBUG)printf("input main: %s",input_ptr);
         memset(input_ptr, 0, strlen(input_ptr) + 1); // check whether +1 is enough to reset whole string
@@ -285,17 +312,9 @@ int main (void) {
             if(DEBUG)printf("Reallocate: new string buffer: %d\n", (int)string_buffer_size);
             char *tmp = malloc(string_buffer_size * sizeof(char));
             getline(&tmp, &len, stdin);
-            /*char* memoryTmp;
-            memoryTmp = realloc(input_ptr, string_buffer_size * sizeof(char) + string_buffer_size / 2);
-            if(memoryTmp == NULL ) {
-                exit(25); //ERROR Code ausgeben
-            }
-            else{
-                input_ptr = memoryTmp;
-            }*/
+            input_ptr = realloc(input_ptr, string_buffer_size * sizeof(char) + string_buffer_size / 2);
             input_ptr = strcat(input_ptr, tmp);
             free(tmp);
-            //free(memoryTmp);
         }
         
         if (input_ptr[0] == 'A') { //exits the reading loop to get into the StartCondition mode
@@ -303,7 +322,15 @@ int main (void) {
         }
         getNode(input_ptr);
     }
-    
+    if(DEBUG) {
+         for(int i = 0; i < nodeCounter; i++){
+        printf("Neighbours von %s am Ende: \n", nodeList[i].name);
+        for(int j = 0; j < (int)nodeList[i].neighbour_count; j++){
+            printf("%s, ", nodeList[i].neighbourList[j]);
+        }
+    }
+    }
+   
     int startNodeId = 0;
     startNodeId = getStartConditions(input_ptr); //line starting with A
     getline(&input_ptr, &len, stdin);
@@ -315,11 +342,11 @@ int main (void) {
         printf("Meine Nodes: \n");
         for (int i = 0; i < nodeCounter; i++)
         {
-            printf("Node: %s, Markierung: %d\n", nodeList[i], (int)marks[i]);
+            printf("Node: %s, Markierung: %d\n", nodeList[i].name, (int)nodeList[i].mark);
         }
         printf("Node Counter: %d\n", nodeCounter);
         printf("Matrix: \n");
-        printMatrix(adjacencyMatrix,BUFFER_SIZE);
+        //printMatrix(adjacencyMatrix,BUFFER_SIZE);
     }
     
     int nextNode = startNodeId;
@@ -330,10 +357,10 @@ int main (void) {
     }
     free(adjacencyMatrix);
     for (int j = 0; j < nodeCounter; j++) {
-        printf("%s:%d\n", nodeList[j], (int)marks[j]);
+        printf("%s:%d\n", nodeList[j].name, (int)nodeList[j].mark);
     }
 
-    printf("E:%s\n", nodeList[nextNode]);
+    printf("E:%s\n", nodeList[nextNode].name);
 
     return 0;
 }
