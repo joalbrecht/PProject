e197e5a245b0e29907a123f15958d1bfa342f7f9
joalbrecht <jonas1albrecht@gmail.com>
2021-08-27 12:05:02 +0200

    no more malloc for inpuut

--- loesung.c
+++ loesung.c
@@ -431,7 +431,7 @@ int main (void) {
   
    
 
-    char *input_ptr = NULL; //malloc(START_BUFFER * sizeof(char*));
+    char *input_ptr; //malloc(START_BUFFER * sizeof(char*));
     nodeList = malloc(BUFFER_SIZE * sizeof(struct Node));
    
     
